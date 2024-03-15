57c036ecf0bdc45b461c6698176ffd2950d2d93e
Kast Bernd <kastbernd@gmx.de>
2024-03-15 21:11:58 +0100

    migrate to python3, add parametrized limits for mbtiles

--- update_maps.sh
+++ update_maps.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # https://www.openstreetmap.org/export#map=19/47.54412/9.68215&layers=N
 
+# Function to run when Ctrl+C is pressed
+function on_interrupt {
+    echo "Ctrl+C pressed. Aborting..."
+    # Your abort command here
+    docker stop seamap_renderer
+    docker stop osm_renderer
+    exit 1  # Exit the script with an error status
+}
+# Trap SIGINT (Ctrl+C) and call the function on_interrupt
+trap on_interrupt SIGINT
+trap on_interrupt  INT
+
 level_start=2
 level_end=19
 latStart=47.82
@@ -30,9 +42,8 @@ name_seamap="OpenSeaMapOfflineLakeConstance"
 docker build -t seamap_renderer -f DockerfileSeamapRenderer .
 docker build -t osm_renderer -f DockerfileOSMRenderer .
 
-# OpenSeamap rendering
+
 mkdir -p ${data_dir}/seamap_tiles/
-docker run -v ${data_dir}:/data seamap_renderer
 echo """
 {
         \"bounds\": [
@@ -50,11 +61,13 @@ echo """
 
 """ > ${data_dir}/seamap_tiles/metadata.json
 
-
-docker run -p 8008:80 -v /data/:/data -e DATA="${data_dir}/data.osm.bz2" osm_renderer &
+# OpenSeamap rendering
+docker run --rm  --name seamap_renderer -v ${data_dir}:/data seamap_renderer &
+# docker run --rm --name  osm_renderer -p 8008:80 -v ${data_dir}/:/data -e DATA="${data_dir}/data.osm.bz2" osm_renderer &
 
 # either directory (possibly with squashfs compression)
 sleep 300
+echo "Start rendering"
 python3 download_tiles.py ${level_start} ${level_end} ${latStart} ${lonStart} ${latEnde} ${lonEnde} "${name_osm}" "${data_dir}/osm_tiles"
 cd ${data_dir}	
 #mksquashfs seamap_tiles osm_tiles offline_tiles.squashfs -comp lz4
@@ -65,11 +78,11 @@ exit 0
 # or mbtiles
 # Openseamap mbtiles
 cd ${data_dir}/seamap_tiles
-python -m SimpleHTTPServer 2> /dev/zero > /dev/zero &
-docker run -v ${data_dir}/:/opt/app/data -e "APP_MODE=command" -e "TILESERVER_TYPE=osm" -e "TILESERVER_ENDPOINT=http://172.17.0.1:8000/{z}/{x}/{y}.png" -e "APP_TIMEOUT=3000" -e "APP_MINZOOM=9" -e "APP_MAXZOOM=18" -e "APP_MAXAREA=160000" mapsquare/mbtiles-generator-server /opt/app/app.sh --left=8.86 --bottom=47.42 --top=47.82 --right=9.8 
+python3 -m http.server 2> /dev/zero > /dev/zero &
+docker run -v ${data_dir}/:/opt/app/data -e "APP_MODE=command" -e "TILESERVER_TYPE=osm" -e "TILESERVER_ENDPOINT=http://172.17.0.1:8000/{z}/{x}/{y}.png" -e "APP_TIMEOUT=3000" -e "APP_MINZOOM=9" -e "APP_MAXZOOM=18" -e "APP_MAXAREA=160000" mapsquare/mbtiles-generator-server /opt/app/app.sh --left=${lonStart} --bottom=${latEnde}  --top=${latStart} --right=${lonEnde}
 
 # Openstreetmap mbtiles
-docker run -v ${data_dir}/:/opt/app/data -e "APP_MODE=command" -e "TILESERVER_TYPE=osm" -e "TILESERVER_ENDPOINT=http://172.17.0.1:8008/hot/{z}/{x}/{y}.png" -e "APP_TIMEOUT=3000" -e "APP_MINZOOM=2" -e "APP_MAXZOOM=19" -e "APP_MAXAREA=160000" mapsquare/mbtiles-generator-server /opt/app/app.sh --left=8.86 --bottom=47.42 --top=47.82 --right=9.8 
+docker run -v ${data_dir}/:/opt/app/data -e "APP_MODE=command" -e "TILESERVER_TYPE=osm" -e "TILESERVER_ENDPOINT=http://172.17.0.1:8008/hot/{z}/{x}/{y}.png" -e "APP_TIMEOUT=3000" -e "APP_MINZOOM=2" -e "APP_MAXZOOM=19" -e "APP_MAXAREA=160000" mapsquare/mbtiles-generator-server /opt/app/app.sh --left=${lonStart} --bottom=${latEnde}--top=${latStart} --right=${lonEnde}
 
 
 
